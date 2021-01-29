0fe2a7d05227abbc5a1bb77b9b7ae384ded974f6
jessennis <jessica.enni@hse.ie>
2021-01-29 16:28:27 +0000

    draft 4

--- Project Draft.py
+++ Project Draft.py
@@ -6,12 +6,13 @@ import seaborn as sns
 
 url = 'https://opendata-geohive.hub.arcgis.com/datasets/d9be85b30d7748b5b7c09450b8aede63_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D'
 urlretrieve(url, 'covid_county.csv')
-covid_df = pd.read_csv('covid_county.csv', index_col = 0)
+covid_df = pd.read_csv('covid_county.csv', index_col=0)
 covid_df.info()
-covid_df2=covid_df.loc[:, ['TimeStamp','CountyName','ConfirmedCovidCases']]
-covid_pt=covid_df2.pivot_table(values='ConfirmedCovidCases', index='TimeStamp', columns='CountyName')
-National=covid_pt.sum(axis="columns")
-covid_pt["National"]= National
+covid_df['TimeStamp']=pd.to_datetime(covid_df['TimeStamp'])
+covid_df = covid_df.loc[:, ['TimeStamp', 'CountyName','ConfirmedCovidCases']]
+covid_pt = covid_df.pivot_table(index=pd.Grouper(freq='W', key='TimeStamp'), columns='CountyName', values='ConfirmedCovidCases', aggfunc='sum')
+National = covid_pt.sum(axis="columns")
+covid_pt["National"] = National
 print(covid_pt)
 
 fig, ax = plt.subplots()
@@ -20,3 +21,4 @@ ax.plot(covid_pt.index, covid_pt["National"])
 ax.set_xlabel('TimeStamp')
 ax.set_ylabel('ConfirmedCovidCases')
 plt.show()
+
