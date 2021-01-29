b922cfb89a60f85b6fef9ec2f73effc0742f4866
jessennis <jessica.enni@hse.ie>
2021-01-29 21:41:00 +0000

    draft 5

--- testing code.py
+++ testing code.py
@@ -8,18 +8,15 @@ url = 'https://opendata-geohive.hub.arcgis.com/datasets/d9be85b30d7748b5b7c09450
 urlretrieve(url, 'covid_county.csv')
 covid_df = pd.read_csv('covid_county.csv', index_col=0)
 
+
 covid_df.info()
 covid_df['TimeStamp']=pd.to_datetime(covid_df['TimeStamp'])
-covid_pt = covid_df.pivot_table(index=pd.Grouper(freq='D', key='TimeStamp'), columns='CountyName', values='ConfirmedCovidCases', aggfunc='sum')
+covid_df = covid_df.loc[:, ['TimeStamp', 'CountyName','ConfirmedCovidCases']]
+covid_pt = covid_df.pivot_table(index=pd.Grouper(freq='W', key='TimeStamp'), columns='CountyName', values='ConfirmedCovidCases', aggfunc='sum')
 National = covid_pt.sum(axis="columns")
 covid_pt["National"] = National
 print(covid_pt)
 
-NewCases_df=covid_pt.loc[:, ['Dublin', 'National']]
-print(NewCases_df)
-print(NewCases_df.diff())
-
-
-
-
-
+for lab, row in covid_pt.iterrows():
+    covid_pt.loc[lab, "TimeStamp"]=len(row["CountyName"])
+    print(covid_pt)
