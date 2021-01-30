b64460232bdff6db1678354cd58e575a30483e78
jessennis <jessica.enni@hse.ie>
2021-01-30 23:23:05 +0000

    sharey

--- Project Draft.py
+++ Project Draft.py
@@ -14,21 +14,17 @@ covid_df['TimeStamp']=pd.to_datetime(covid_df['TimeStamp'])
 covid_pt = covid_df.pivot_table(index=pd.Grouper(freq='D', key='TimeStamp'), columns='CountyName', values='ConfirmedCovidCases', aggfunc='sum')
 National = covid_pt.sum(axis="columns")
 covid_pt["National"] = National
+covid_pt["Dub_percentage"]=covid_pt['Dublin']/covid_pt['National']
 print(covid_pt)
+
 
 df=covid_pt.loc[:, ['Dublin', 'National']]
 print(df)
 NewCases_df=df.diff()
 print(NewCases_df)
-NewCases_df["Dub_percentage"]=NewCases_df['Dublin']/NewCases_df['National']
-print(NewCases_df)
-
 print(NewCases_df.isna().sum())
 NewCases_df2=NewCases_df.fillna(0)
-print(NewCases_df2)
 
-NewCases_df2['Dub_percentage'] = pd.Series(["{0:.2f}%".format(val * 100) for val in NewCases_df2['Dub_percentage']], index = NewCases_df2.index)
-print(NewCases_df2)
 
 fig1, ax = plt.subplots()
 ax.plot(NewCases_df2.index, NewCases_df2["Dublin"], color='b')
@@ -38,7 +34,9 @@ ax.set_ylabel("New Covid Cases per day")
 ax.set_title("New Covid Cases")
 plt.show()
 
-fig2, ax = plt.subplots(2,1)
+fc=plt.subplots
+
+fig2, ax = fc(2,1)
 ax[0].plot(NewCases_df2.index, NewCases_df2["Dublin"], color='b')
 ax[1].plot(NewCases_df2.index, NewCases_df2["National"], color='red')
 ax[1].set_xlabel("Time (Months)")
@@ -58,7 +56,7 @@ Pop_df2=Pop_df.fillna(0)
 print(Pop_df2)
 
 
-fig3, ax = plt.subplots()
+fig3, ax = fc()
 ax.plot(Pop_df2.index, Pop_df2["Dublin"], color='b')
 ax.plot(Pop_df2.index, Pop_df2["National"], color='red')
 ax.set_xlabel("Time (Months)")
@@ -66,7 +64,7 @@ ax.set_ylabel("New Covid Cases per Proportion of Population per day")
 ax.set_title("New Covid Cases per Proportion of Population ")
 plt.show()
 
-fig4, ax = plt.subplots(2,1, sharey=True)
+fig4, ax = fc(2,1, sharey=True)
 ax[0].plot(NewCases_df2.index, NewCases_df2["Dublin"], color='b')
 ax[0].plot(NewCases_df2.index, NewCases_df2["National"], color='red'),
 ax[1].plot(Pop_df2.index, Pop_df2["Dublin"], color='b')
@@ -75,9 +73,3 @@ ax[0].set_xlabel("Time (Months)")
 ax[0].set_ylabel("New Covid Cases per day")
 ax[1].set_ylabel("New Covid Cases per Proportion of Population per day")
 plt.show()
-
-Merged_df=NewCases_df2.merge(Pop_df2, on='TimeStamp', how='right')
-print(Merged_df)
-print(Merged_df.columns)
-print(Merged_df[["Dublin_x", "National_x", "Dublin_y", "National_y","Dub_percentage"]].max())
-print(Merged_df[["Dublin_x", "National_x", "Dublin_y", "National_y","Dub_percentage"]].min())
