72464f91f55b1c0d15da73d50ef4ad635199c0c1
jessennis <jessica.enni@hse.ie>
2021-01-30 10:59:16 +0000

    format &

--- Project Draft.py
+++ Project Draft.py
@@ -3,6 +3,7 @@ import pandas as pd
 from urllib.request import urlretrieve
 import matplotlib.pyplot as plt
 import seaborn as sns
+import jinja2
 
 url = 'https://opendata-geohive.hub.arcgis.com/datasets/d9be85b30d7748b5b7c09450b8aede63_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D'
 urlretrieve(url, 'covid_county.csv')
@@ -21,8 +22,14 @@ df=covid_pt.loc[:, ['Dublin', 'National']]
 print(df)
 NewCases_df=df.diff()
 print(NewCases_df)
+NewCases_df["Dub_percentage"]=NewCases_df['Dublin']/NewCases_df['National']
+print(NewCases_df)
+
 print(NewCases_df.isna().sum())
 NewCases_df2=NewCases_df.fillna(0)
+print(NewCases_df2)
+
+NewCases_df2['Dub_percentage'] = pd.Series(["{0:.2f}%".format(val * 100) for val in NewCases_df2['Dub_percentage']], index = NewCases_df2.index)
 print(NewCases_df2)
 
 
