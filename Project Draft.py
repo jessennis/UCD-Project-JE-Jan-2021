26a0eebbce7845fd303fd34f7353a91e9509d364
jessennis <jessica.enni@hse.ie>
2021-01-31 16:47:44 +0000

    Populations added

--- Project Draft.py
+++ Project Draft.py
@@ -13,8 +13,8 @@ covid_df['TimeStamp']=pd.to_datetime(covid_df['TimeStamp'])
 covid_pt = covid_df.pivot_table(index=pd.Grouper(freq='D', key='TimeStamp'), columns='CountyName', values='ConfirmedCovidCases', aggfunc='sum')
 National = covid_pt.sum(axis="columns")
 covid_pt["National"] = National
-covid_pt["Dub_percentage"]=covid_pt['Dublin']/covid_pt['National']
 print(covid_pt)
+
 
 fc=plt.subplots
 
@@ -22,7 +22,7 @@ fig0, ax = fc()
 ax.plot(covid_pt.index, covid_pt["Dublin"], color='b', label='Dublin')
 ax.plot(covid_pt.index, covid_pt["National"], color='r', label ='National')
 ax.set_xlabel("Time (Months)")
-ax.set_ylabel("Confirmed Covid Cases per day")
+ax.set_ylabel("Confirmed Covid Cases")
 ax.legend(loc="upper center")
 ax.set_title("Confirmed Covid Cases")
 plt.show()
@@ -46,20 +46,6 @@ ax.legend(loc="upper center")
 plt.show()
 fig1.savefig("New Covid Cases")
 
-fc=plt.subplots
-
-fig2, ax = fc(2,1, sharex=True)
-fig2.suptitle('New Covid Cases V2')
-ax[1].plot(NewCases_df2.index, NewCases_df2["Dublin"], color='b',label='Dublin')
-ax[0].plot(NewCases_df2.index, NewCases_df2["National"], color='r',label='National')
-ax[1].set_xlabel("Time (Months)")
-ax[0].set_ylabel("New Covid Cases")
-ax[1].set_ylabel("New Covid Cases")
-ax[0].legend(loc="upper center")
-ax[1].legend(loc="upper center")
-plt.show()
-fig2.savefig('New Covid Cases V2')
-
 
 pop_pt = covid_df.pivot_table(index=pd.Grouper(freq='D', key='TimeStamp'), columns='CountyName', values='PopulationProportionCovidCases', aggfunc='sum')
 National = pop_pt.sum(axis="columns")
@@ -73,19 +59,30 @@ print(Pop_df.isna().sum())
 Pop_df2=Pop_df.fillna(0)
 print(Pop_df2)
 
+population_pt = covid_df.pivot_table(columns='CountyName', values='PopulationCensus16')
+National = population_pt.sum(axis="columns")
+population_pt["National"] = National
+print(population_pt)
+df2=population_pt.loc[:, ['Dublin', 'National']]
+print(df2)
 
-fig3, ax = fc()
+
+fig2, ax = fc()
+fig2.suptitle('Proportion of COVID Cases per Population')
 ax.plot(Pop_df2.index, Pop_df2["National"], color='r', label='National')
-ax.plot(Pop_df2.index, Pop_df2["Dublin"], color='b', label='Dublin')
 ax.set_xlabel("Time (Months)")
-ax.set_ylabel("Proportion of COVID Cases per Population per day")
-ax.set_title("Proportion of COVID Cases per Population")
+ax.set_ylabel("National - Proportion of COVID Cases per Population per day")
 ax.legend(loc="upper center")
+ax2= ax.twinx()
+ax2.plot(Pop_df2.index, Pop_df2["Dublin"], color='b', label='Dublin')
+ax2.set_xlabel("Time (Months)")
+ax2.set_ylabel("Dublin- Proportion of COVID Cases per Population per day")
+ax2.legend(loc="upper left")
 plt.show()
-fig3.savefig("Proportion of COVID Cases per Population")
+fig2.savefig("Proportion of COVID Cases per Population")
 
-fig4, ax = fc(2, sharex=True)
-fig4.suptitle('New COVID Cases vs Proportion of New COVID Cases per Population')
+fig3, ax = fc(2, sharex=True)
+fig3.suptitle('New COVID Cases vs Proportion of New COVID Cases per Population')
 ax[0].plot(NewCases_df2.index, NewCases_df2["National"], color='r',label='National'),
 ax[0].plot(NewCases_df2.index, NewCases_df2["Dublin"], color='b', label='Dublin')
 ax[1].plot(Pop_df2.index, Pop_df2["Dublin"], color='b', label='Dublin')
@@ -96,4 +93,4 @@ ax[1].set_ylabel("Proportion per Population")
 ax[0].legend(loc="upper center")
 plt.show()
 
-fig4.savefig("New COVID Cases vs Proportion of New COVID Cases per Population")
+fig3.savefig("New COVID Cases vs Proportion of New COVID Cases per Population")
